5db53ebeb9238830f3408d57f3c2d868a5134c4c
Andy <andy.french@hotmail.co.uk>
2022-08-04 16:30:42 +0100

    Inital commit

---
+++ app/components/Container.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+
+// change from small to large screen size
+
+function Container(props) {
+  return (
+    <div
+      className={"container py-md-5 " + (props.wide ? "" : "container--narrow")}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default Container;
